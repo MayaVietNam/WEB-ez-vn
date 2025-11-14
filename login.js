1bc766406c606c5a886b97a6d7ee18e43fc9b573
MayaVietNam <ezfibymaya@gmail.com>
2025-11-14 13:27:29 +0700

    ffdf

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const corpForm = document.getElementById('corp-form');
 
   // [QUAN TRỌNG] Dán URL ứng dụng web (Web App URL) của bạn vào đây
-  const LOGIN_SCRIPT_URL = 'https://script.google.com/macros/s/xxxxxxxxxxxx/exec'; 
+  const LOGIN_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxCEzp3E_n1B167Lfg4wfgoY1YMGTX9pPXwoU92EDZY5haOlxhjcIZdAt789qKfM86v/exec'; 
   // (Hãy chắc chắn URL này là URL triển khai của file Code.gs đã sửa lỗi CORS)
 
   /**
